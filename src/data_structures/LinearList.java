85c0be262721e82d62821845506379e255ed686b
hjuarez <23268165+whyrll@users.noreply.github.com>
2019-02-28 13:45:53 -0800

    Create LinearList class

---
+++ src/data_structures/LinearList.java
@@ -0,0 +1,96 @@
+package data_structures;
+
+import java.util.Iterator;
+
+public class LinearList<E extends Comparable<E>> implements LinearListADT<E> {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean addFirst(E obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addLast(E obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E remove(E obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peekFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peekLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(E obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E find(E obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
