06dada2264d46d44b26f66426b8786ce17022f2a
hjuarez <23268165+whyrll@users.noreply.github.com>
2019-02-28 15:39:02 -0800

    Replicate comments from LinearListADT; add modificationCounter to modifying methods

--- src/data_structures/LinearList.java
+++ src/data_structures/LinearList.java
@@ -45,9 +45,11 @@ public class LinearList<E extends Comparable<E>> implements LinearListADT<E> {
 	@Override
 	public boolean addFirst(E obj) {
 		Node<E> newNode = new Node<E>(obj);
-		
+
 		newNode.next = this.head;
 		this.head = newNode;
+		this.currentSize++;
+		this.modificationCounter++;
 		return true;
 	}
 
@@ -58,78 +60,134 @@ public class LinearList<E extends Comparable<E>> implements LinearListADT<E> {
 	@Override
 	public boolean addLast(E obj) {
 		Node<E> newNode = new Node<E>(obj);
-		
 		newNode.previous = this.tail;
 		this.tail = newNode;
+		this.currentSize++;
+		this.modificationCounter++;
 		return true;
 	}
 
+	/*
+	 * Removes and returns the parameter object obj in first position in list if the
+	 * list is not empty, null if the list is empty.
+	 */
 	@Override
 	public E removeFirst() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.isEmpty())
+			return null;
+		E dataToReturn = this.head.data;
+		this.head = this.head.next;
+		this.currentSize--;
+		this.modificationCounter++;
+		return (E) dataToReturn;
 	}
 
+	/*
+	 * Removes and returns the parameter object obj in last position in list if the
+	 * list is not empty, null if the list is empty.
+	 */
 	@Override
 	public E removeLast() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.isEmpty())
+			return null;
+		E dataToReturn = this.tail.data;
+		this.tail = this.tail.previous;
+		this.currentSize--;
+		this.modificationCounter++;
+		return (E) dataToReturn;
 	}
 
+	/*
+	 * Removes and returns the parameter object obj from the list if the list
+	 * contains it, null otherwise. The ordering of the list is preserved. The list
+	 * may contain duplicate elements. This method removes and returns the first
+	 * matching element found when traversing the list from first position. Note
+	 * that you may have to shift elements to fill in the slot where the deleted
+	 * element was located.
+	 */
 	@Override
 	public E remove(E obj) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/*
+	 * Returns the first element in the list, null if the list is empty. The list is
+	 * not modified.
+	 */
 	@Override
 	public E peekFirst() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.head.data;
 	}
 
+	/*
+	 * Returns the last element in the list, null if the list is empty. The list is
+	 * not modified.
+	 */
 	@Override
 	public E peekLast() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.tail.data;
 	}
 
+	/*
+	 * Returns true if the parameter object obj is in the list, false otherwise. The
+	 * list is not modified.
+	 */
 	@Override
 	public boolean contains(E obj) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/*
+	 * Returns the element matching obj if it is in the list, null otherwise. In the
+	 * case of duplicates, this method returns the element closest to front. The
+	 * list is not modified.
+	 */
 	@Override
 	public E find(E obj) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/*
+	 * The list is returned to an empty state.
+	 */
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		this.head = this.tail = null;
+		this.currentSize = 0;
+		this.modificationCounter++;
 	}
 
+	/*
+	 * Returns true if the list is empty, otherwise false
+	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.currentSize == 0;
 	}
 
+	/*
+	 * Returns true if the list is full, otherwise false
+	 */
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/*
+	 * Returns the number of Objects currently in the list.
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.currentSize;
 	}
 
+	/*
+	 * Returns an Iterator of the values in the list, presented in the same order as
+	 * the underlying order of the list. (front first, rear last)
+	 */
 	@Override
 	public Iterator<E> iterator() {
 		// TODO Auto-generated method stub
